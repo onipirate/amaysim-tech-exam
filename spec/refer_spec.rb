321d4825572654bf9bb7e2544c2bb4cbdaa5db85
John Erwin Chan <jcsserv@gmail.com>
2022-11-27 20:55:30 +0800

    modified rspec to use let

--- spec/refer_spec.rb
+++ spec/refer_spec.rb
@@ -1,24 +1,22 @@
 require_relative '../lib/refer.rb'
 
-
 RSpec.describe 'Amaysim Refer a friend' do
 
   let(:env) {'https://www.amaysim.com.au'}
-
-  it 'is accessible when you login' do
+  let(:refer) do
     @refer=Refer.new(env)
     @refer.login_as('0466134574','AWqasde321')
     @refer.navigate_to_refer_friend_page
+  end
+
+  it 'is accessible when you login' do
+    refer
     expect(@refer.url_page).to include('https://www.amaysim.com.au/my-account/my-amaysim/refer_friends')
   end
 
   context 'Can Send an Invite Email' do
-    before(:example) do
-      @refer=Refer.new(env)
-      @refer.login_as('0466134574','AWqasde321')
-      @refer.navigate_to_refer_friend_page     
-    end
-  
+    before(:example) { refer }
+
     it 'using 1 email address' do
       @refer.email_set('jcsupp@gmail.com')
       expect(@refer.message).to eq('THANKS FOR SHARING THE BIG LOVE')
